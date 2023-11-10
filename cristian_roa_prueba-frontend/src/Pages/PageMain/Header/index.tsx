5ce28b3caf4b868d153d832e702e4a32ca04e514
RRCris <curiosity.cdrr@gmail.com>
2023-11-10 05:50:30 -0500

    add component Image for lazy loading nad placholder image

--- cristian_roa_prueba-frontend/src/Pages/PageMain/Header/index.tsx
+++ cristian_roa_prueba-frontend/src/Pages/PageMain/Header/index.tsx
@@ -1,5 +1,22 @@
 import React from "react";
 import "./style.scss";
-export default function Header() {
-  return <div className="Header">Header</div>;
+import Image from "@/components/Image";
+interface PropsHeader {
+  title?: string;
+  subtitle?: string;
+  description?: string;
+  images?: string[];
+}
+export default function Header(props: PropsHeader) {
+  if (!props.title) return <h1>load...</h1>;
+  return (
+    <div className="Header">
+      <div className="containerImages">
+        <Image
+          src={props.images ? props.images[0] : ""}
+          alt="imagen de portada"
+        />
+      </div>
+    </div>
+  );
 }
---
+++ cristian_roa_prueba-frontend/src/components/Image/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import placeHolderImage from "@/assets/placeHolderImageDark.webp";
+
+interface PropsImage {
+  src: string;
+  alt: string;
+  width?: number;
+  height?: number;
+}
+export default function Image(props: PropsImage) {
+  return (
+    <LazyLoadImage
+      src={props.src}
+      alt={props.alt}
+      width={props.width}
+      height={props.height}
+      effect="blur"
+      placeholderSrc={placeHolderImage}
+    />
+  );
+}
